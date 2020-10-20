8fdf1483b8a64712821254d933d704208685bba0
Ashley Holanda <aholanda12@gmail.com>
2020-10-20 12:11:58 -0400

    styled the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./style.css";
 
 function Header(props) {
-  return  <div class="sky">
-  <div class="clouds"></div>
+  return  <div className="sky">
+  <div className="clouds"></div>
   <h1 className="title">{props.children}</h1>
+  <h2 className="subtitle">Full Stack Developer</h2>
 </div>;
 }
 
