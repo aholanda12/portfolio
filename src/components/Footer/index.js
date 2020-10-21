7a982d0bdebd5d7f16f83225bc83e93ced00a63c
Ashley Holanda <aholanda12@gmail.com>
2020-10-20 22:17:13 -0400

    added links to the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,11 +11,11 @@ function Footer() {
   return (
     <footer className="footer">
       <span>&copy; 2020 Ashley Holanda
-         <a href="" className="icon"><FaRegFilePdf /></a>
-         <a href="" className="icon"><SiLinkedin /></a>
-         <a href="" className="icon"><SiGithub /></a>
-         <a href="" className="icon"><FaFacebook /></a>
-         <a href="" className="icon"><AiOutlineMail /></a>
+         <a href="/" className="icon"><FaRegFilePdf /></a>
+         <a href="https://www.linkedin.com/in/ashley-holanda-0585671a4/" className="icon"><SiLinkedin /></a>
+         <a href="https://github.com/aholanda12" className="icon"><SiGithub /></a>
+         <a href="https://www.facebook.com/ashley.holanda" className="icon"><FaFacebook /></a>
+         <a href="mailto:aholanda12@gmail.com" className="icon"><AiOutlineMail /></a>
         </span>
     </footer>
   );
