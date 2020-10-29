1e554eea26701647678212e2d5060b1742173b0c
Ashley Holanda <aholanda12@gmail.com>
2020-10-29 15:15:32 -0400

    Updates

---
+++ precache-manifest.d1ea2a9b2ed4ea658aef03b410e5cca8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28b90a5207e10ee3e217771e489a1ae",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3e7f927a427567ad2d11",
+    "url": "/portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d69587534d37fb898366",
+    "url": "/portfolio/static/css/main.2c0c91ca.chunk.css"
+  },
+  {
+    "revision": "3e7f927a427567ad2d11",
+    "url": "/portfolio/static/js/2.533f3683.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.533f3683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69587534d37fb898366",
+    "url": "/portfolio/static/js/main.f62423a9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "76671af35fbe5152a72d16a11abe57ac",
+    "url": "/portfolio/static/media/clearsky.76671af3.png"
+  },
+  {
+    "revision": "994c0b2f9592eed3fb9de9dfc803b692",
+    "url": "/portfolio/static/media/cloud.994c0b2f.png"
+  }
+]);
